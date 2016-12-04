05726bdc0a01c32ef43e67b5e721105b0df86d6b
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2016-12-04 20:34:15 +0800

    render json to view wip

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,10 @@
 # myapp.rb
 require 'sinatra'
+require 'json'
+require 'ostruct'
 
 get '/' do
+  file = File.read('resume.json')
+  @data = JSON.parse(file, object_class: OpenStruct)
   erb :index
 end
